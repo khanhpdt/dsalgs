13d3527dbca0a8afad040ea2c6723fc7e6c2bc24
khanhpdt <tungkhanh.a2@gmail.com>
2016-09-21 09:52:21 +0700

    - Remove unused field

--- data-structures/src/main/java/vn/khanhpdt/playgrounds/datastructures/stacks/Stack.java
+++ data-structures/src/main/java/vn/khanhpdt/playgrounds/datastructures/stacks/Stack.java
@@ -2,23 +2,17 @@ package vn.khanhpdt.playgrounds.datastructures.stacks;
 
 import vn.khanhpdt.playgrounds.datastructures.nodes.LinkedNodeIntf;
 
-import java.util.UUID;
-
 /**
  * @param <N> type of node
  * @author khanhpdt
  */
 public class Stack<N extends LinkedNodeIntf<N>> {
 
-	// used to identify this stack
-	private final UUID uuid;
-
 	private N head;
 
 	private int size;
 
 	public Stack() {
-		this.uuid = UUID.randomUUID();
 		this.head = null;
 		this.size = 0;
 	}
