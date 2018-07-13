2cfeea59b93d1baa6321617d04cac9a201d7ed83
Khanh Pham <khanhpdtvn@gmail.com>
2018-07-13 11:33:37 +0700

    Insertion sort v2: items are shifted when searching for the right position

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -24,3 +24,30 @@ def insertion_sort(items):
 class InsertionSortTest(SortTestBase):
     def sorting_func(self, items):
         return insertion_sort(items)
+
+
+def insertion_sort_2(items):
+    print("Sorting [%s]..." % list_to_str(items))
+
+    items_copy = items.copy()
+
+    for i in range(len(items_copy)):
+        item = items_copy[i]
+
+        # find the position to insert items[i] without breaking the order in the sorted area
+        j = i - 1
+        while j >= 0 and items_copy[j] > item:
+            # This is the difference b/w this function and the insertion_sort() function above.
+            # shift item to the right to preserve place to insert the item
+            items_copy[j + 1] = items_copy[j]
+
+            j = j - 1
+
+        items_copy[j + 1] = item
+
+    return items_copy
+
+
+class InsertionSort2Test(SortTestBase):
+    def sorting_func(self, items):
+        return insertion_sort_2(items)
--- sorting/test_sorting.py
+++ sorting/test_sorting.py
@@ -8,12 +8,14 @@ class SortTestBase(TestCase):
         raise RuntimeError("Not supported")
 
     def test_simple(self):
-        self.assertEqual(self.sorting_func([]), [])
-        self.assertEqual(self.sorting_func([1]), [1])
-        self.assertEqual(self.sorting_func([4, 1, 2, 3]), [1, 2, 3, 4])
+        self.assertEqual([], self.sorting_func([]))
+        self.assertEqual([1], self.sorting_func([1]))
+        self.assertEqual([1, 2, 3, 4], self.sorting_func([1, 2, 3, 4]))
+        self.assertEqual([1, 2, 3, 4], self.sorting_func([4, 3, 2, 1]))
+        self.assertEqual([4, 4, 4, 4], self.sorting_func([4, 4, 4, 4]))
 
     def test_many(self):
         for i in range(1, 100):
             arr = list(range(1, i + 1))
             random.shuffle(arr)
-            self.assertEqual(self.sorting_func(arr), sorted(arr))
+            self.assertEqual(sorted(arr), self.sorting_func(arr))
