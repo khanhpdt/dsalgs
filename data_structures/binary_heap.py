afde60f131361225d83dee7a654650a0cdabcb22
Khanh Pham <khanhpdtvn@gmail.com>
2018-07-16 11:47:47 +0700

    Refactor binary_heap

--- data_structures/binary_heap.py
+++ data_structures/binary_heap.py
@@ -1,29 +1,76 @@
+from abc import ABC, abstractmethod
 from math import floor
 
 from common.utils import exchange
 
 
+class BinaryHeapProperty(ABC):
+
+    @abstractmethod
+    def hold_for_item(self, item, parent):
+        raise NotImplementedError("Not supported")
+
+    def hold_for_array(self, arr):
+        i = 0
+        while 2 * i + 1 < len(arr):
+            child_1 = 2 * i + 1
+            if not self.hold_for_item(arr[child_1], arr[i]):
+                print("Child %s > Node %s" % (child_1, i))
+                return False
+
+            child_2 = 2 * i + 2
+            if child_2 < len(arr) and not self.hold_for_item(arr[child_2], arr[i]):
+                print("Child %s > Node %s" % (child_2, i))
+                return False
+
+            i = i + 1
+
+        return True
+
+
+class MinHeapProperty(BinaryHeapProperty):
+
+    def hold_for_item(self, item, parent):
+        return parent <= item
+
+
+class MaxHeapProperty(BinaryHeapProperty):
+
+    def hold_for_item(self, item, parent):
+        return parent >= item
+
+
 class BinaryHeap:
 
-    def __init__(self) -> None:
-        self.items = []
+    def __init__(self, heap_property: BinaryHeapProperty) -> None:
+        self._heap_property = heap_property
+        self._items = []
 
     def build(self, arr):
-        self.items = []
+        self._items = []
         for item in arr:
-            self.items.append(item)
-            self.ensure_heap_property(len(self.items) - 1)
+            self._items.append(item)
+            self._ensure_heap_property(len(self._items) - 1)
 
-    def ensure_heap_property(self, item_idx):
+    def _ensure_heap_property(self, item_idx):
         parent_idx = self._get_parent_of(item_idx)
-        while parent_idx >= 0 and self.items[item_idx] > self.items[parent_idx]:
-            exchange(self.items, item_idx, parent_idx)
+        while parent_idx >= 0 and not self._heap_property.hold_for_item(self.get(item_idx), self.get(parent_idx)):
+            exchange(self._items, item_idx, parent_idx)
             item_idx = parent_idx
             parent_idx = self._get_parent_of(item_idx)
+
+    def get(self, item_idx):
+        return self._items[item_idx]
+
+    def size(self):
+        return len(self._items)
 
     @staticmethod
     def _get_parent_of(i):
         return int(floor((i - 1) / 2))
 
-    def to_array(self):
-        return self.items.copy()
+    def _to_array(self):
+        return self._items.copy()
+
+    def check_heap_property(self):
+        return self._heap_property.hold_for_array(self._to_array())
--- data_structures/test/binary_heap_test.py
+++ data_structures/test/binary_heap_test.py
@@ -1,49 +1,77 @@
 import random
 
-from data_structures.binary_heap import BinaryHeap
+from data_structures.binary_heap import BinaryHeap, MinHeapProperty, MaxHeapProperty
 
 
-class TestBinaryHeap:
+class TestBinaryMinHeapProperty:
+
+    heap_property = None
+
+    def setup_class(self):
+        self.heap_property = MinHeapProperty()
+
+    def test_check_node(self):
+        assert self.heap_property.hold_for_item(2, 1)
+        assert not self.heap_property.hold_for_item(1, 2)
+
+    def test_check_heap(self):
+        assert self.heap_property.hold_for_array([1, 2, 3])
+        assert self.heap_property.hold_for_array([1, 3, 2])
+        assert not self.heap_property.hold_for_array([2, 1, 3])
+        assert not self.heap_property.hold_for_array([1, 2, 7, 4, 5, 6])
+        assert self.heap_property.hold_for_array([1, 2, 7, 4, 5, 8])
+
+
+class TestBinaryMaxHeapProperty:
+
+    heap_property = None
+
+    def setup_class(self):
+        self.heap_property = MaxHeapProperty()
+
+    def test_check_node(self):
+        assert self.heap_property.hold_for_item(1, 2)
+        assert not self.heap_property.hold_for_item(2, 1)
+
+    def test_check_heap(self):
+        assert self.heap_property.hold_for_array([3, 2, 1])
+        assert self.heap_property.hold_for_array([3, 1, 2])
+        assert not self.heap_property.hold_for_array([2, 1, 3])
+        assert not self.heap_property.hold_for_array([7, 6, 4, 3, 2, 5])
+        assert self.heap_property.hold_for_array([7, 6, 4, 3, 2, 1])
+
+
+class BinaryHeapTestCommon:
 
     heap: BinaryHeap = None
 
-    def setup_class(self):
-        self.heap = BinaryHeap()
-
     def test_build_heap_empty(self):
         self.heap.build([])
-        assert self.satisfy_max_heap_property()
+        assert self.heap.check_heap_property()
 
     def test_build_heap_one_element(self):
         self.heap.build([1])
-        assert self.satisfy_max_heap_property()
+        assert self.heap.check_heap_property()
 
     def test_build_heap_three_elements(self):
         self.heap.build([2, 1, 3])
-        assert self.satisfy_max_heap_property()
+        assert self.heap.check_heap_property()
 
     def test_build_heap_many(self):
         for i in range(100):
             arr = list(range(i))
             random.shuffle(arr)
             self.heap.build(arr)
-            assert self.satisfy_max_heap_property()
+            assert self.heap.check_heap_property()
 
-    def satisfy_max_heap_property(self):
-        arr = self.heap.to_array()
 
-        i = 0
-        while 2 * i + 1 < len(arr):
-            child_1 = 2 * i + 1
-            if arr[child_1] > arr[i]:
-                print("Child %s > Node %s" % (child_1, i))
-                return False
+class TestBinaryMaxHeap(BinaryHeapTestCommon):
 
-            child_2 = 2 * i + 2
-            if child_2 < len(arr) and arr[child_2] > arr[i]:
-                print("Child %s > Node %s" % (child_2, i))
-                return False
+    def setup_class(self):
+        self.heap = BinaryHeap(MaxHeapProperty())
 
-            i = i + 1
 
-        return True
+class TestBinaryMinHeap(BinaryHeapTestCommon):
+
+    def setup_class(self):
+        self.heap = BinaryHeap(MinHeapProperty())
