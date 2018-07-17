158368cb796923020347cc35338d9fef334e5cc5
Khanh Pham <khanhpdtvn@gmail.com>
2018-07-17 10:54:40 +0700

    Mergesort topdown

---
+++ tests/sorting/merge_sort_test.py
@@ -0,0 +1,21 @@
+from src.sorting.merge_sort import merge, merge_sort_top_down
+from tests.sorting.sort_test_common import SortTestCommon
+
+
+class TestMerge:
+
+    def test_merge(self):
+        arr = [3, 7, 19, 5, 8, 10, 12, 2, 1]
+        merge(arr, 0, 2, 5)
+        assert arr[0:6] == [3, 5, 7, 8, 10, 19]
+
+    def test_merge_whole_array(self):
+        arr = [3, 7, 19, 5, 8, 10]
+        merge(arr, 0, 2, 5)
+        assert arr == [3, 5, 7, 8, 10, 19]
+
+
+class TestMergeSortTopDown(SortTestCommon):
+
+    def sort_func(self, items):
+        return merge_sort_top_down(items)
