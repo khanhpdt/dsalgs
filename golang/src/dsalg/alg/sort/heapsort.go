d12235744bdc7581fbbf48efcfe8514ee5b3afe3
Khanh Pham <khanhpdtvn@gmail.com>
2020-04-11 14:38:06 +0700

    Add comments

--- golang/src/dsalg/alg/sort/heapsort.go
+++ golang/src/dsalg/alg/sort/heapsort.go
@@ -5,13 +5,14 @@ import "dsalg/ds/heap"
 // HeapSort sorts the given items in ascending order.
 // Complexity: O(Nlog(N)), where N is the number of items to sort.
 func HeapSort(items []string) []string {
-	h := heap.NewMaxHeap(items) // O(N)
+	// build the initial max heap (O(N))
+	h := heap.NewMaxHeap(items)
 
 	n := h.Len()
 	for i := h.Len() - 1; i >= 1; i-- {
-		h.Exchange(0, i) // move the largest item to last
-		n--
-		h.Sink(0, n) // ensure max-heap property
+		h.Exchange(0, i) // move the largest item to the end of the heap
+		n--              // reduce heap size as the last items are already sorted
+		h.Sink(0, n)     // ensure max-heap property
 	}
 
 	sorted := h.Keys()
