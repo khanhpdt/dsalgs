d3ce4282b3c19f1a22df72269e5415b6a0ec3cb1
Khanh Pham <khanhpdtvn@gmail.com>
2020-04-11 16:31:35 +0700

    Mergesort

--- golang/src/dsalg/alg/sort/sort_test.go
+++ golang/src/dsalg/alg/sort/sort_test.go
@@ -5,25 +5,33 @@ import (
 	"testing"
 )
 
-func TestHeapSort(t *testing.T) {
-	tests := []struct {
-		input []string
-		want  []string
-	}{
-		{input: nil, want: nil},
-		{input: []string{}, want: []string{}},
-		{input: []string{"c"}, want: []string{"c"}},
-		{input: []string{"c", "b"}, want: []string{"b", "c"}},
-		{input: []string{"b", "c", "a"}, want: []string{"a", "b", "c"}},
-		{input: []string{"b", "c", "a", "d"}, want: []string{"a", "b", "c", "d"}},
-		{input: []string{"b", "c", "e", "a", "d"}, want: []string{"a", "b", "c", "d", "e"}},
-		{input: []string{"b", "e", "c", "a", "f", "d"}, want: []string{"a", "b", "c", "d", "e", "f"}},
-	}
+var tests = []struct {
+	input []string
+	want  []string
+}{
+	{input: nil, want: nil},
+	{input: []string{}, want: []string{}},
+	{input: []string{"c"}, want: []string{"c"}},
+	{input: []string{"c", "b"}, want: []string{"b", "c"}},
+	{input: []string{"b", "c", "a"}, want: []string{"a", "b", "c"}},
+	{input: []string{"b", "c", "a", "d"}, want: []string{"a", "b", "c", "d"}},
+	{input: []string{"b", "c", "e", "a", "d"}, want: []string{"a", "b", "c", "d", "e"}},
+	{input: []string{"b", "e", "c", "a", "f", "d"}, want: []string{"a", "b", "c", "d", "e", "f"}},
+}
 
+func runTest(t *testing.T, sortFunc func([]string) []string) {
 	for _, test := range tests {
-		sorted := HeapSort(test.input)
+		sorted := sortFunc(test.input)
 		if !slice.Equal(sorted, test.want) {
 			t.Errorf("got = %v, want = %v", sorted, test.want)
 		}
 	}
+}
+
+func TestHeapSort(t *testing.T) {
+	runTest(t, HeapSort)
+}
+
+func TestMergeSort(t *testing.T) {
+	runTest(t, MergeSort)
 }
